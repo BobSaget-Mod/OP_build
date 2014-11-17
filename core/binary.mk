1822c247097124237a99e50b73f57f4e7e5ca817
Paul Beeler <pbeeler80@gmail.com>
2014-11-16 21:30:18 -0700

    Add graphite flags
    Change-Id: I90bd416e800936b2130b82246fe0fd047774d466
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/binary.mk
+++ core/binary.mk
@@ -97,6 +97,9 @@ else
   endif
 endif
 
+# Include graphite flags in all builds.
+include $(BUILD_SYSTEM)/graphite.mk
+
 # The following LOCAL_ variables will be modified in this file.
 # Because the same LOCAL_ variables may be used to define modules for both 1st arch and 2nd arch,
 # we can't modify them in place.
