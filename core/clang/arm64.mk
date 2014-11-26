b3107f8e911db50143a7944bbcbb5cc7873f2c1c
Chet Kener <Cl3Kener@gmail.com>
2014-11-26 02:01:47 -0500

    Filter Out More
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/clang/arm64.mk
+++ core/clang/arm64.mk
@@ -16,6 +16,24 @@ CLANG_CONFIG_arm64_UNKNOWN_CFLAGS := \
   -frename-registers \
   -fno-strict-volatile-bitfields \
   -fno-align-jumps \
+  -frename-registers \
+  -fno-builtin-sin \
+  -fno-caller-saves \
+  -fno-early-inlining \
+  -fno-move-loop-invariants \
+  -fno-partial-inlining \
+  -fno-strict-volatile-bitfields \
+  -fno-tree-copy-prop \
+  -fno-tree-loop-optimize \
+  -funsafe-loop-optimizations \
+  -fno-vect-cost-model \
+  -fno-ipa-cp-clone \
+  -fsection-anchors \
+  -ftree-loop-im \
+  -ftree-loop-ivcanon \
+  -fgcse-sm \
+  -fgcse-las \
+  -fweb \
   -Wa,--noexecstack
 
 # We don't have any arm64 flags to substitute yet.
--- core/clang/x86_64.mk
+++ core/clang/x86_64.mk
@@ -10,7 +10,35 @@ CLANG_CONFIG_x86_64_UNKNOWN_CFLAGS := \
   -finline-limit=300 \
   -fno-inline-functions-called-once \
   -mfpmath=sse \
-  -mbionic
+  -mbionic \
+  -Wno-unused-but-set-variable \
+  -Wno-unused-but-set-parameter \
+  -Wmaybe-uninitialized \
+  -Wno-maybe-uninitialized \
+  -Wno-error=maybe-uninitialized \
+  -fno-canonical-system-headers \
+  -mthumb-interwork \
+  -fgcse-after-reload \
+  -frerun-cse-after-loop \
+  -frename-registers \
+  -fno-align-jumps \
+  -fno-builtin-sin \
+  -fno-caller-saves \
+  -fno-early-inlining \
+  -fno-move-loop-invariants \
+  -fno-partial-inlining \
+  -fno-strict-volatile-bitfields \
+  -fno-tree-copy-prop \
+  -fno-tree-loop-optimize \
+  -funsafe-loop-optimizations \
+  -fno-vect-cost-model \
+  -fno-ipa-cp-clone \
+  -fsection-anchors \
+  -ftree-loop-im \
+  -ftree-loop-ivcanon \
+  -fgcse-sm \
+  -fgcse-las \
+  -fweb
 
 # We don't have any x86_64 flags to substitute yet.
 define subst-clang-incompatible-x86_64-flags
