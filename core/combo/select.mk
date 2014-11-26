95d82b4f7ddd6dddeb74d082ba5c0ff76e4479d4
Chet Kener <Cl3Kener@gmail.com>
2014-11-26 01:39:08 -0500

    Strict Aliasing on select.mk too
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -49,9 +49,9 @@ $(combo_var_prefix)HAVE_STRLCPY := 0
 $(combo_var_prefix)HAVE_STRLCAT := 0
 $(combo_var_prefix)HAVE_KERNEL_MODULES := 0
 
-$(combo_var_prefix)GLOBAL_CFLAGS := -O3 -DNDEBUG -fivopts -ffunction-sections -fdata-sections -funswitch-loops -fomit-frame-pointer -ftracer -Wno-error=unused-parameter -Wno-error=unused-but-set-variable -Wno-error=maybe-uninitialized -fno-exceptions -Wno-multichar -pipe
-$(combo_var_prefix)RELEASE_CFLAGS := -O3 -DNDEBUG -fno-strict-aliasing -fivopts -ffunction-sections -fdata-sections -funswitch-loops -fomit-frame-pointer -ftracer -Wno-error=unused-parameter -Wno-error=unused-but-set-variable -Wno-error=maybe-uninitialized -pipe
-$(combo_var_prefix)GLOBAL_CPPFLAGS := -O3 -DNDEBUG -fivopts -ffunction-sections -fdata-sections -funswitch-loops -pipe -fomit-frame-pointer -ftracer -Wno-error=unused-parameter -Wno-error=unused-but-set-variable -Wno-error=maybe-uninitialized
+$(combo_var_prefix)GLOBAL_CFLAGS := -O3 -DNDEBUG -fstrict-aliasing  -fivopts -ffunction-sections -fdata-sections -funswitch-loops -fomit-frame-pointer -ftracer -Wno-error=unused-parameter -Wno-error=unused-but-set-variable -Wno-error=maybe-uninitialized -fno-exceptions -Wno-multichar -pipe
+$(combo_var_prefix)RELEASE_CFLAGS := -O3 -DNDEBUG -fstrict-aliasing -fivopts -ffunction-sections -fdata-sections -funswitch-loops -fomit-frame-pointer -ftracer -Wno-error=unused-parameter -Wno-error=unused-but-set-variable -Wno-error=maybe-uninitialized -pipe
+$(combo_var_prefix)GLOBAL_CPPFLAGS := -O3 -DNDEBUG -fstrict-aliasing -fivopts -ffunction-sections -fdata-sections -funswitch-loops -pipe -fomit-frame-pointer -ftracer -Wno-error=unused-parameter -Wno-error=unused-but-set-variable -Wno-error=maybe-uninitialized
 $(combo_var_prefix)GLOBAL_LDFLAGS := -Wl,-O1 -Wl,--as-needed -Wl,--relax -Wl,--sort-common -Wl,--gc-sections
 $(combo_var_prefix)GLOBAL_ARFLAGS := crsPD
 $(combo_var_prefix)GLOBAL_LD_DIRS :=
