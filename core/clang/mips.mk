54b7436b993d04b3bf9298c63c74b9bbd5e6d0c3
Chet Kener <Cl3Kener@gmail.com>
2014-11-26 02:39:49 -0500

    Filter Out Clang on Mips 2
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/clang/mips.mk
+++ core/clang/mips.mk
@@ -11,8 +11,27 @@ CLANG_CONFIG_mips_UNKNOWN_CFLAGS := \
   -fgcse-after-reload \
   -frerun-cse-after-loop \
   -frename-registers \
+  -fno-align-jumps \
+  -fno-builtin-sin \
+  -fno-caller-saves \
+  -fno-early-inlining \
+  -fno-move-loop-invariants \
+  -fno-partial-inlining \
+  -fno-strict-volatile-bitfields \
+  -fno-tree-copy-prop \
+  -fno-tree-loop-optimize \
+  -funsafe-loop-optimizations \
+  -fno-vect-cost-model \
+  -fno-ipa-cp-clone \
+  -fsection-anchors \
+  -ftree-loop-im \
+  -ftree-loop-ivcanon \
+  -fgcse-sm \
+  -fgcse-las \
+  -fweb \
   -msynci \
-  -mno-fused-madd
+  -mno-fused-madd \
+
 
 # We don't have any mips flags to substitute yet.
 define subst-clang-incompatible-mips-flags
--- core/clang/mips64.mk
+++ core/clang/mips64.mk
@@ -11,6 +11,24 @@ CLANG_CONFIG_mips64_UNKNOWN_CFLAGS := \
   -fgcse-after-reload \
   -frerun-cse-after-loop \
   -frename-registers \
+  -fno-align-jumps \
+  -fno-builtin-sin \
+  -fno-caller-saves \
+  -fno-early-inlining \
+  -fno-move-loop-invariants \
+  -fno-partial-inlining \
+  -fno-strict-volatile-bitfields \
+  -fno-tree-copy-prop \
+  -fno-tree-loop-optimize \
+  -funsafe-loop-optimizations \
+  -fno-vect-cost-model \
+  -fno-ipa-cp-clone \
+  -fsection-anchors \
+  -ftree-loop-im \
+  -ftree-loop-ivcanon \
+  -fgcse-sm \
+  -fgcse-las \
+  -fweb \
   -msynci \
   -mno-fused-madd
 
