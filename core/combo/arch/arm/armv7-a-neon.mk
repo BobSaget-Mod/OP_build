218a64f657c8b19dbea82a43de48dd5ae673f974
Jake Weinstein <xboxfanj@msn.com>
2014-12-22 13:01:03 -0500

    build: clang: use swift and VFP4 flags for krait
    Change-Id: I4c22d6d7d8d2dcf5de26ba3061bba58c74eaf93c
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -12,9 +12,9 @@ ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VA
 	#       krait CPU variant added to GCC/clang.
 	arch_variant_cflags := -mcpu=cortex-a15
 
-	# Fake an ARM compiler flag as these processors support LPAE which GCC/clang
-	# don't advertise.
-	arch_variant_cflags += -D__ARM_FEATURE_LPAE=1
+	# Fake ARM compiler flags as these processors support LPAE and VFP4 which GCC/clang
+	# doesn't advertise.
+	arch_variant_cflags += -D__ARM_FEATURE_LPAE=1 -D__ARM_FEATURE_VFP4=1
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
 	arch_variant_cflags := -mcpu=cortex-a9
