ce30849f0c936fa9606062334bbc79eb627b4185
Chet Kener <Cl3Kener@gmail.com>
2014-12-30 16:46:13 -0500

    Attempt to tune for Krait Modularly
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/binary.mk
+++ core/binary.mk
@@ -132,6 +132,10 @@ endif
 endif
 endif
 
+ifeq ($(KRAIT_TUNINGS),true)
+include $(BUILD_SYSTEM)/krait.mk
+endif
+
 # The following LOCAL_ variables will be modified in this file.
 # Because the same LOCAL_ variables may be used to define modules for both 1st arch and 2nd arch,
 # we can't modify them in place.
