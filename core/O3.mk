d2afc5ea0155ebcb5a92caf9a35f93bcd7a0521e
Chet Kener <Cl3Kener@gmail.com>
2015-01-02 21:57:00 -0500

    Remove webview related files from -O3 and GCC Only Optimizations
    This causes FCs on some applications.  I haven't experienced it but it makes no sense to really make these files larger anyways.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/O3.mk
+++ core/O3.mk
@@ -21,6 +21,9 @@ LOCAL_DISABLE_O3 := \
 	libstagefright_webm \
 	libjavacore \
 	net_net_gyp \
+	libwebviewchromium \
+	libwebviewchromium_loader \
+	libwebviewchromium_plat_support \
 	content_content_renderer_gyp \
 	third_party_WebKit_Source_modules_modules_gyp \
 	third_party_WebKit_Source_platform_blink_platform_gyp \
